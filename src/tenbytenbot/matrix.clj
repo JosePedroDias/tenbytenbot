b1aa51b3cd5b1e186e39ca13dd502e9f1687c765
José Pedro <jose.pedro.dias@gmail.com>
2015-08-11 00:52:50 +0100

    yay

--- src/tenbytenbot/matrix.clj
+++ src/tenbytenbot/matrix.clj
@@ -12,26 +12,39 @@
   (+ (get pos 0) (* (get pos 1) (get dims 0))))
 
 
-(defn mcreate [dims positions]
+;;-------------------
+
+
+(defn mcreate
+  [dims]
   "creates a matrix of m by n dims, with false values"
-  (let [matrix {:dims dims :values (-create (reduce * dims))}]
-    matrix))
+  {:dims dims :values (-create (reduce * dims))})
 
 
 (defn mget [matrix pos]
+  "gets element at position pos"
   (let [values (get matrix :values) dims (get matrix :dims)]
     (get values (-linearize dims pos))))
 
 
 (defn mset [matrix pos val]
+  "sets element at position pos with value val"
   (let [values (get matrix :values) dims (get matrix :dims)]
     (assoc
       matrix
-      (assoc values (-linearize dims pos) val)
-      :values)))
+      :values
+      (assoc values (-linearize dims pos) val))))
+
+
+(defn msets [matrix positions val]
+  "same as mset but accepts sequence of elements"
+  (if (empty? positions)
+    matrix
+    (msets (mset matrix (first positions) val) (next positions) val)))
 
 
 (defn mprint [matrix]
+  "prints matrix"
   (let [values (get matrix :values) dims (get matrix :dims)]
     (doseq [y (range (get dims 1)) x (range (get dims 0))]
       (let [v (mget matrix [x y])]
@@ -40,4 +53,20 @@
           (print "\n"))))))
 
 
-(mprint (mcreate [3 2] (list [0 0] [2 1])))
+;(mcreate [3 2])
+
+;(mset (mcreate [3 2]) [0 0] true)
+
+(msets (mcreate [3 2]) '() true)
+(msets (mcreate [3 2]) '([0 0]) true)
+(msets (mcreate [3 2]) '([0 0] [0 1]) true)
+
+;(msets (mcreate [3 2]) '([0 0] [1 1]) true)
+
+;(mprint (mcreate [3 2]))
+
+;(mset (mcreate [3 2]) [0 0] true)
+
+;(mprint (mcreate [3 2] (list [0 0])))
+
+;(mset(mcreate [3 2])
