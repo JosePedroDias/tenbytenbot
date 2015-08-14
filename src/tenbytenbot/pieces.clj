bf9c5cc34429dc38df90a843f08cbfbc1c0e5db6
José Pedro <jose.pedro.dias@gmail.com>
2015-08-14 03:25:49 +0100

    added valid pieces and random-piece defn

--- src/tenbytenbot/pieces.clj
+++ src/tenbytenbot/pieces.clj
@@ -48,6 +48,45 @@
 
 
 
+;; (def -odds
+;;   [
+;;     4       ; 1
+;;     2 2     ; 2
+;;     2 2     ; 3
+;;     1 1 1 1 ; 4
+;;     2 2     ; 5
+;;     4       ; 6
+;;     2 2     ; 7
+;;     4       ; 8
+;;     1 1 1 1 ; 9
+;; ])
+
+
+
+(def -piece-mapping
+  [
+     0  0  0  0
+     1  1  2  2
+     3  3  4  4
+     5  6  7  8
+     9  9 10 10
+    11 11 11 11
+    12 12 13 13
+    14 14 14 14
+    15 16 17 18
+])
+
+
+
+(defn random-piece []
+  "NON-DETERMINISTIC BEHAVIOR!
+  returns random piece with same probability between 9 available kinds"
+  (get
+    pieces
+    (get
+      -piece-mapping
+      (rand-int 36))))
+
 ;(get pieces 1)
 
 ;(count pieces)
@@ -55,3 +94,5 @@
 ;(doseq [p pieces]
 ;  (print "\n")
 ;  (mprint p))
+
+;(random-piece)
