21046d104782169841bf9e04552d8f130a096dbb
José Pedro <jose.pedro.dias@gmail.com>
2015-08-17 08:25:13 +0100

    play works

--- src/tenbytenbot/tactics.clj
+++ src/tenbytenbot/tactics.clj
@@ -6,35 +6,48 @@
 
 
 
-(defn play [board slot-nums]
-  ""
-  (let [slots (map (fn [num] (get p/pieces num)) slot-nums)]
+(defn play [board slot-nums debug]
+  "elects a move to perform"
+  (let [slots (vec (map (fn [num] (get p/pieces num)) slot-nums))]
 
-    ;(println slots)
-    (println "----")
-    (doseq [[i slot] (map-indexed vector slots)]
-      (println (str "\n" i ":"))
-      (m/mprint slot))
+    (when debug
+      (println "----")
+      (doseq [[i slot] (map-indexed vector slots)]
+        (println (str "\n" i ":"))
+        (m/mprint slot)))
 
     (let [pos [0 0]
           slot-index 0
           slot (get slots slot-index)]
-      (println (str "\nabout to play " slot-index " on " pos "..."))
-      {:slot-index slot-index :pos pos}
-      ;(io/play )
-      ;(m/mprint (m/glue board slot pos))
-      )))
+
+      (when debug
+        (println (str "\nabout to play " slot-index " on " pos "...")))
+
+      { :slot slot
+        :slot-index slot-index
+        :pos pos})))
 
 
 
 (defn go []
   "play a tenbyten game"
-  (let [board (m/mcreate [10 10])
+  (let [debug true
+        board (m/mcreate [10 10])
         ng (io/new-game)]
-    (println ng)
 
-    (let [p (play board (:slots ng))
+    (when debug
+      (println ng))
+
+    (let [p (play board (:slots ng) debug)
           [x y] (:pos p)]
 
-      (let [r (io/play (:id ng) (:step ng) (:slot-index p) x y)]
-        (println r)))))
+      (let [r (io/play (:id ng) (:step ng) (:slot-index p) x y)
+            board2 (m/glue board (:slot p) (:pos p))]
+        (when debug
+          (println r)
+          (println)
+          (m/mprint (:slot p))
+          (println)
+          (m/mprint board2))
+
+        ))))
