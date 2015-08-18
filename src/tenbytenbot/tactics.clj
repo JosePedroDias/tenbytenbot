1365721f4d6bf1803bd134eb4a485c25de15039b
José Pedro <jose.pedro.dias@gmail.com>
2015-08-18 19:05:18 +0100

    erm almost plays well

--- src/tenbytenbot/tactics.clj
+++ src/tenbytenbot/tactics.clj
@@ -6,17 +6,38 @@
 
 
 
+(defn compute-valid-positions-for-piece [board piece]
+  "returns the sequence of positions a piece can occupy on the given board"
+  (let [[bw bh] (:dims board)
+        [pw ph] (:dims piece)]
+    (filter
+      (fn [pos] (not (m/collides-with board piece pos)))
+      (for [x (range (inc (- bw pw)))
+            y (range (inc (- bh ph)))]
+        [x y]))))
+
+
+(defn compute-valid-moves [board slotnums]
+  "returns a sequence valid moves with keys for :slot-index and :pos"
+  (apply concat
+    (map
+      (fn [[index slotnum]]
+        (let [piece (get p/pieces slotnum)
+              positions (compute-valid-positions-for-piece board piece)]
+          (map
+            (fn [pos] {:slot-index index :pos pos})
+            positions)))
+      (filter
+        (fn [[index slotnum]] slotnum)
+        (map vector (iterate inc 0) slotnums)))))
+
+
+;(compute-valid-moves (m/mcreate [5 5]) [3 nil 6])
+
+
 (defn play [board slot-nums step]
-  "elects a move to perform"
-  (let [slots (vec (map (fn [num] (get p/pieces num)) slot-nums))]
-
-    (let [pos [(* step 3) 0]
-          slot-index step
-          slot (get slots slot-index)]
-
-      { :slot slot
-        :slot-index slot-index
-        :pos pos})))
+  (let [valid-moves (compute-valid-moves board slot-nums)]
+    (rand-nth valid-moves)))
 
 
 
@@ -38,18 +59,19 @@
         (println))
 
       (let [move (play board (:slots state) (:step state))
+            piece (get p/pieces (get (:slots state) (:slot-index move)))
             [x y] (:pos move)]
 
         (when verbose
           (println (str "About to play slot #" (:slot-index move) " on pos " x ", " y ))
 
-          (when (:slot move)
+          (when piece
             (println)
-            (m/mprint (:slot move))
+            (m/mprint piece)
             (println)))
 
         (let [new-board (try
-                          (m/glue board (:slot move) (:pos move))
+                          (m/glue board piece (:pos move))
                           (catch Exception ex
                             ;(println ex)
                             (println "invalid move!")
